088eeedb3955781bee9cc3c59445578af8709dd7
i am vip pro <vitoiconchoi@gmail.com>
2021-07-30 19:56:03 +0700

    s

--- url.js
+++ url.js
@@ -3,6 +3,7 @@ document.getElementById("receiverAddress").addEventListener("keydown", event => 
     console.log(event.key)
     if(event.key=="/")
     {
-        alert("dấu /"); alert("dấu /");
+        alert("dấu / (tự xóa)"); 
+        return false;
     }
 });
