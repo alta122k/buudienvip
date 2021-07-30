63b1057356b4d1dbd083fa864926fe05bba7c416
i am vip pro <vitoiconchoi@gmail.com>
2021-07-30 19:49:03 +0700

    e

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
-$.getScript("https://code.jquery.com/jquery-3.6.0.min.js");
+
 $("input").on ('keypress', function (e) {
     console.log(String.fromCharCode(e.which));
     if(String.fromCharCode(e.which)=="/")
