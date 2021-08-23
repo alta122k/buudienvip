8cb6962b3eaf85c42445ae8cfbd7e780951dc0a6
i am vip pro <vitoiconchoi@gmail.com>
2021-08-23 17:59:53 +0700

    ư

--- pns.js
+++ pns.js
@@ -2,13 +2,16 @@ function copyText(e) {
     if(e.html()!="")
     {
             var textBox = $(".clipboard");
+            
             textBox.val(e.html())
             textBox.select();
+
             document.execCommand('copy');
             var colorR = Math.floor((Math.random() * 256));
             var colorG = Math.floor((Math.random() * 256));
             var colorB = Math.floor((Math.random() * 256));
             $('.tr_head').css("background", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+            $('td a').css("color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
             cuteToast({
                 type: "success", // or 'info', 'error', 'warning'
                 message: "Đã copy "+e.html(),
