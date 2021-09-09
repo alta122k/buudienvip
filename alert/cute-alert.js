854a53a2a41f93a8eb4ab3d4fc26514ce80b5987
i am vip pro <vitoiconchoi@gmail.com>
2021-09-09 19:16:02 +0700

    s

--- alert/cute-alert.js
+++ alert/cute-alert.js
@@ -152,3 +152,54 @@ function cuteToast({ type, message, timer = 5000 }) {
     });
   });
 }
+
+
+function cuteToastshake({ type, message, timer = 5000 }) {
+  return new Promise((resolve) => {
+    if (document.querySelector(".toast-container")) {
+      document.querySelector(".toast-container").remove();
+    }
+    const body = document.querySelector("body");
+
+    const scripts = document.getElementsByTagName("script");
+    let currScript = "";
+
+    for (let script of scripts) {
+      if (script.src.includes("cute-alert.js")) {
+        currScript = script;
+      }
+    }
+
+    let src = currScript.src;
+
+    src = src.substring(0, src.lastIndexOf("/"));
+
+    const template = `
+    <div class="toast-container ${type}-bg">
+      <div>
+        <div class="toast-frame">
+          <span class="shake-slow shake-constant toast-message">${message}</span>
+          <div class="toast-close">X</div>
+        </div>
+        <div class="toast-timer ${type}-timer" style="animation: timer ${timer}ms linear;"/>
+      </div>
+    </div>
+    `;
+
+    body.insertAdjacentHTML("afterend", template);
+
+    const toastContainer = document.querySelector(".toast-container");
+
+    setTimeout(() => {
+      toastContainer.remove();
+      resolve();
+    }, timer);
+
+    const toastClose = document.querySelector(".toast-close");
+
+    toastClose.addEventListener("click", () => {
+      toastContainer.remove();
+      resolve();
+    });
+  });
+}
