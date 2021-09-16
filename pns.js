f7b4d71f6f9308070735369af240efc52bcdd7b4
i am vip pro <vitoiconchoi@gmail.com>
2021-09-16 19:54:38 +0700

    s

--- pns.js
+++ pns.js
@@ -18,11 +18,11 @@ function copyText(e) {
             textBox.select();
 
             document.execCommand('copy');
-            var colorR = Math.floor((Math.random() * 256));
-            var colorG = Math.floor((Math.random() * 256));
-            var colorB = Math.floor((Math.random() * 256));
-            $('.tr_head').css("background", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
-            $('td a').css("color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+            // var colorR = Math.floor((Math.random() * 256));
+            // var colorG = Math.floor((Math.random() * 256));
+            // var colorB = Math.floor((Math.random() * 256));
+            // $('.tr_head').css("background", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+            // $('td a').css("color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
            
     }
 
