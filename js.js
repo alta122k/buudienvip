ff7f1b83120cff13dce691f711aa08043a68e427
i am vip pro <vitoiconchoi@gmail.com>
2021-10-07 17:44:23 +0700

    d

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ $("form .form-group button").ready(function () {
             na.document.write('<link rel="stylesheet" type="text/css" href="https://alta122k.github.io/buudienvip/pnw.css">\
             <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">\
             <title>Bưu điện xin cảm ơn</title>\
+            <link href="https://pns.vnpost.vn/Content/assets/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">\
             <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>\
             <script src="https://alta122k.github.io/buudienvip/pns.js"></script>\
             <script src="https://alta122k.github.io/buudienvip/alert/cute-alert.js"></script>\
